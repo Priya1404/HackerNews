e890835d032f59117992b332cc4035a987a87b0e
Priya Srivastava <priya.srivastava@arvindinternet.com>
2021-09-12 19:53:28 +0530

    Added NetworkManager and URLManager classes

---
+++ PlayoHackerNews/Common/NetworkManager.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkManager.swift
+//  PlayoHackerNews
+//
+//  Created by Priya Srivastava on 12/09/21.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let sharedInstance = NetworkManager()
+    
+    func request <Model: Codable> (url: String, success: @escaping (Model) -> Void, failure: @escaping (Error) -> Void) {
+        guard let uRL = URL(string: url) else {
+            return
+        }
+        let session = URLSession.shared.dataTask(with: uRL) { data, response, error in
+            do {
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                    failure(error)
+                } else if let data = data {
+                    let jsonRes = try JSONSerialization.jsonObject(with: data, options: [])
+                    print("Response: \(jsonRes)")
+                    let json = try JSONDecoder().decode(Model.self, from: data)
+                    success(json)
+                }
+            } catch let error {
+                failure(error)
+            }
+        }
+        session.resume()
+    }
+}
---
+++ PlayoHackerNews/Common/URLManager.swift
@@ -0,0 +1,23 @@
+//
+//  URLManager.swift
+//  PlayoHackerNews
+//
+//  Created by Priya Srivastava on 12/09/21.
+//
+
+import Foundation
+
+class URLManager {
+    
+    static let sharedInstance = URLManager()
+    
+    private let BASEURL = Bundle.main.infoDictionary?["MY_API_BASE_URL_ENDPOINT"] as? String ?? "https://hn.algolia.com/"
+
+    enum APIType: String {
+        case searchText = "api/v1/search?query=<%@>"
+    }
+    
+    public func getApiURLForType(apiType : APIType) -> String{
+        return BASEURL + apiType.rawValue
+    }
+}
